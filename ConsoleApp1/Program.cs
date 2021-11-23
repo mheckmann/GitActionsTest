4eaded2239e8563976d351e695c595cde9d61d27
Matt Heckmann <183231+mheckmann@users.noreply.github.com>
2021-11-22 20:29:58 -0500

    initial commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+            Console.ReadKey(true);
+        }
+    }
+}
